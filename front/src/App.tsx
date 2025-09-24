6c2f73291b8884a387f03884ab379041b7ace363
BeatrizMoreira12 <beatrizmoreiraoficial@gmail.com>
2025-09-23 21:07:40 -0300

    organização do projeto com as pastas e inclusão do stlyed components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import MainContent from './components/Main/MainContent';
+import AddTask from './pages/AddTask/AddTask';
+import TaskList from './pages/TaskList/TaskList';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Task {
+  id: number;
+  name: string;
 }
 
-export default App
+function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (taskName: string) => {
+    setTasks([...tasks, { id: tasks.length + 1, name: taskName }]);
+  };
+
+  const removeTask = (taskId: number) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  };
+
+  return (
+    <div className="app-container">
+      <Header />
+      <MainContent>
+        <h1>Conteúdos</h1>
+        <AddTask onAddTask={addTask} />
+        <TaskList tasks={tasks} onRemoveTask={removeTask} />
+      </MainContent>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
